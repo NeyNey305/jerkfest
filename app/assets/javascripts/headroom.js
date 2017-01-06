70d205b31c0513ad704e7a6c01503508e0cd7440
Jenee Washington <jeneewashington@icloud.com>
2017-01-06 15:49:09 -0500

    Working on mobile responsiveness

---
+++ app/assets/javascripts/headroom.js
@@ -0,0 +1,6 @@
+// grab an element
+var headroom--unpinned = document.querySelector("header");
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(myElement);
+// initialise
+headroom.init();
